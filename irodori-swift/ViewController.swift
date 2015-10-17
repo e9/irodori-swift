61e749a0fd1435655efc4490c11938de53f5c1b5
Shotaro Osaki <t05181so@gmail.com>
2015-10-18 03:21:36 +0900

    first commit

---
+++ irodori-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  irodori-swift
+//
+//  Created by Shotaro Osaki on 2015/10/18.
+//  Copyright © 2015年 chabolab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
