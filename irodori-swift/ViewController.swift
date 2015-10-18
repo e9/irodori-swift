aae435f5d0e6a2c18366a03609a6d94acf4255a5
Shotaro Osaki <t05181so@gmail.com>
2015-10-19 03:47:16 +0900

    実機のカメラにいろどりフィルタを掛けたい。

--- irodori-swift/ViewController.swift
+++ irodori-swift/ViewController.swift
@@ -16,20 +16,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        let sourceImage = UIImage(named: "dummy")
-        let imagePicture = GPUImagePicture(image: sourceImage)
-        let irodoriFilter = GPUImageIrodoriFilter()
         
+        let imageView = GPUImageView.init(frame: self.view.frame)
+        self.view.addSubview(imageView)
+        
+        let irodoriFilter = GPUImageIrodoriFilter()
         irodoriFilter.setHue(340.0, max: 365.0)
         irodoriFilter.setBr(0.4, max: 1.0)
         irodoriFilter.setSat(0.5, max: 1.0)
-        imagePicture.addTarget(irodoriFilter)
-        imagePicture.processImage()
         
-        let destinationImage = irodoriFilter.imageByFilteringImage(sourceImage)
-        self.view.addSubview(UIImageView(image: destinationImage))
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
+            let stillCamera = GPUImagePicture(image: UIImage(named: "dummy"))
+        #else
+            let stillCamera = GPUImageStillCamera()
+            // stillCamera.outputImageOrientation = .Portrait
+            // stillCamera.shouldSmoothlyScaleOutput = true
+        #endif
         
+        stillCamera.addTarget(irodoriFilter)
+        irodoriFilter.addTarget(imageView)
+        
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
+            stillCamera.processImage()
+        #else
+            stillCamera.startCameraCapture()
+        #endif
+        
+        /*
         Alamofire
             .request(.GET, "http://irodori-server.dev/photos/red.json")
             .validate()
@@ -37,6 +50,7 @@ class ViewController: UIViewController {
                 let json = JSON(response.result.value!)
                 print(json[0]["user"]["name"])
             }
+        */
     }
 
     override func didReceiveMemoryWarning() {
