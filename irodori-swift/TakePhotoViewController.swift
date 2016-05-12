993dd52415c64560ddfc0870c09f6081750a8ed6
Chabose <chabose@gmail.com>
2016-05-13 00:57:20 +0900

    Implement Touch Focus/Exposure

--- irodori-swift/TakePhotoViewController.swift
+++ irodori-swift/TakePhotoViewController.swift
@@ -23,7 +23,7 @@ class TakePhotoViewController: UIViewController {
     @IBOutlet weak var retakePhotoButton: UIButton!
     @IBOutlet weak var takePhotoButton: UIButton!
     @IBOutlet weak var imageView: GPUImageView!
-
+    
     
     var isPhotoTaken:Bool = false {
         didSet{
@@ -38,49 +38,53 @@ class TakePhotoViewController: UIViewController {
         
     }
     
-
-
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         startCamera();
         /*
-        Alamofire
-            .request(.GET, "http://irodori-server.dev/photos/red.json")
-            .validate()
-            .responseJSON { response in
-                let json = JSON(response.result.value!)
-                print(json[0]["user"]["name"])
-            }
-        */
+         Alamofire
+         .request(.GET, "http://irodori-server.dev/photos/red.json")
+         .validate()
+         .responseJSON { response in
+         let json = JSON(response.result.value!)
+         print(json[0]["user"]["name"])
+         }
+         */
     }
     
-
-
+    
+    //on Shutter touched
     @IBAction func shutterTouchUpInside(sender: AnyObject) {
         
         if isPhotoTaken==false {
             
             stillCamera.capturePhotoAsImageProcessedUpToFilter(irodoriFilter) { (image, error) in
-            print("takephoto");
-            
-            self.suspendCamera()
-            self.isPhotoTaken = true
-
-        }
+                print("takephoto");
+                
+                self.suspendCamera()
+                self.isPhotoTaken = true
+                
+            }
         }else{
             
         }
         
-
-        
     }
+    
+    // on Retake touched
     @IBAction func touchRetake(sender: AnyObject) {
         self.isPhotoTaken = false
         self.resumeCamera()
         
     }
+    
+    
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "takePhoto"{
@@ -89,14 +93,14 @@ class TakePhotoViewController: UIViewController {
             nextViewController.gpuImage = sender as? GPUImagePicture;
         }
     }
-
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+    // camera initialization
     func startCamera(){
         irodoriFilter.setHue(340.0, max: 365.0)
         irodoriFilter.setBr(0.4, max: 1.0)
@@ -135,8 +139,47 @@ class TakePhotoViewController: UIViewController {
         
     }
     
+    //Change focus/exposure to designated point
+    func setFocusandExposure(pointofInterest:CGPoint)->(){
+        do{
+            try stillCamera.inputCamera.lockForConfiguration()
+            
+            if stillCamera.inputCamera.isFocusModeSupported(AVCaptureFocusMode.AutoFocus){
+                stillCamera.inputCamera.focusPointOfInterest = pointofInterest;
+                stillCamera.inputCamera.focusMode = AVCaptureFocusMode.AutoFocus;
+            }
+            
+            if stillCamera.inputCamera.isExposureModeSupported(AVCaptureExposureMode.AutoExpose){
+                stillCamera.inputCamera.exposurePointOfInterest = pointofInterest;
+                stillCamera.inputCamera.exposureMode = AVCaptureExposureMode.AutoExpose;
+            }
+            
+        }catch{
+            // err handling
+        }
+        
+        stillCamera.inputCamera.unlockForConfiguration()
+    }
     
-
-
+    // detect touchEnd on Screen and conver it to ImageView's screen
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        let x:CGFloat? = touches.first?.locationInView(self.imageView).x
+        let y:CGFloat? = touches.first?.locationInView(self.imageView).x
+        
+        
+        //Convert CGRect to float for focus
+        let px = x!/self.imageView.frame.size.width;
+        let py = y!/self.imageView.frame.size.height;
+        
+        let pointofInterest=CGPointMake(py,1.0-px);
+        
+        if(px<=1 && py<=1){
+            self.setFocusandExposure(pointofInterest)
+        }
+    }
+    
+    
+    
 }
 
